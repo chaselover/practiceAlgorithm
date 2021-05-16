c6faaa07eaf7d5b9445c5e09ffa8e2b6f0a38a37
SeungJoo Han <79824434+chaselover@users.noreply.github.com>
2021-05-16 23:49:14 +0900

    Add files via upload

---
+++ double_linkedList.py
@@ -0,0 +1,13 @@
+class HashTable: 
+    def __init__(self): 
+        self.hash_table = list([0 for i in range(8)]) 
+    def hash_function(self, key): 
+        return key % 8 
+    def insert(self, key, value): 
+        hash_value = self.hash_function(hash(key)) 
+        self.hash_table[hash_value] = value 
+    def read(self, key): 
+        hash_value = self.hash_function(hash(key)) 
+        return self.hash_table[hash_value] 
+    def print(self): print(self.hash_table)
+
