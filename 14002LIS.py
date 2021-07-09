b990bf9d001c4a1cea2f4838965e733ab4fb275b
SeungJoo Han <gkstmdwn234@gmail.com>
2021-07-09 19:12:38 +0900

    Add files via upload

--- 14002LIS.py
+++ 14002LIS.py
@@ -9,12 +9,13 @@ for i in range(1, N):
             dp[i] = max(dp[i], dp[j]+1)
 
 print(max(dp))
+
 order = max(dp)
 lst = []
 for i in range(N-1, -1, -1):
     if dp[i]==order:
         lst.append(arr[i])
         order-=1
-lst.reverse()
-for i in lst:
-    print(i, end=' ')
+# lst.reverse()
+while lst:
+    print(lst.pop(), end=' ')
