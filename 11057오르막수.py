3800b98bf7b479bd7440eeba745439437b39f871
SeungJoo Han <gkstmdwn234@gmail.com>
2021-07-10 19:17:35 +0900

    Add files via upload

---
+++ 11057오르막수.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+matrix=[[1]*10 for _ in range(n)]
+
+for i in range(1,n):
+    sum=0
+    for j in range(10):
+        for k in range(j+1):
+            matrix[i][j] += matrix[i-1][k]
+            sum += matrix[i-1][k]
+
+if n==1:
+    print(10)
+else:
+    print(sum%10007)
