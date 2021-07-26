97166263bab7e1089cfe3e84d6a51b26746beda7
chaselover <gkstmdwn234@gmail.com>
2021-07-26 15:45:07 +0900

    0726daily Algo03

--- 20007떡돌리기.py
+++ 20007떡돌리기.py
@@ -10,9 +10,8 @@ def dijkstra(start):
     while heap:
         cur_dist,cur_node = heappop(heap)
         for next_node,next_dists in graph[cur_node]:
-            dists = cur_dist + next_dists
-            if dp_dists[next_node] > dp_dists[cur_node] + dists:
-                dp_dists[next_node] = dp_dists[cur_node] + dists
+            if dp_dists[next_node] > cur_dist + next_dists:
+                dp_dists[next_node] = cur_dist + next_dists
                 heappush(heap,[dp_dists[next_node],next_node])
 
 
@@ -22,21 +21,30 @@ def dijkstra(start):
 # 집은 0~N-1번까지.
 N,M,X,Y = map(int,input().split())
 graph = {i:[] for i in range(N)}
-dp_dists = {i:float('inf') for i in range(N)}
+dp_dists = [float('inf') for _ in range(N)]
 
 for _ in range(M):
     a,b,c = map(int,input().split())
     graph[a].append([b,c])
     graph[b].append([a,c])
 
-dijkstra(0)
+dijkstra(Y)
 
 # 2. 최소거리*2가 X이상이면 -1출력
-for house in dp_dists.values():
-    if house > 2*X:
+for i in range(N):
+    if dp_dists[i] > X//2:
         print(-1)
         exit()
 
-
-# 3. 최소거리*2의 합이 X이하인 조합의 최소갯수.
-# 4. 내림차순 정렬하고 아래로 타고 내려가면서 X까지 채우는 식으로. 냅색 0-1문제이므로 2차원 dp활용?
+# 3. 최소거리*2의 합이 X이하인 조합의 최소갯수.//가까운 곳부터 방문 -> 정렬
+# 4. 오름차순 그냥 더하는 식으로.(가까운 곳 부터)
+dp_dists.sort()
+daily_move = 0
+day = 1
+for i in range(N):
+    if (daily_move + dp_dists[i])*2 <= X:
+        daily_move = daily_move + dp_dists[i]
+    else:
+        daily_move = dp_dists[i]
+        day+=1
+print(day)
