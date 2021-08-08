847eb8d496b931a1ce8dd8a71f1e0a2d6288566e
chaselover <gkstmdwn234@gmail.com>
2021-08-08 17:31:56 +0900

    Feat daily algo

--- 1316그룹단어체커.py
+++ 1316그룹단어체커.py
@@ -14,3 +14,11 @@ for _ in range(N):
         cnt+=1
 print(cnt)
     
+
+# 빠른풀이. sort의 key 값을 이용해 정렬 순서를 조작한다. s.find즉 빨리나오는 알파뱃의 인덱스 순서대로 정렬(그룹단어체크를 강제로 만든다.)
+# res = 0
+# for i in range(int(input())):
+#     s = input()
+#     if list(s) == sorted(s, key=s.find):
+#         res += 1
+# print(res)
