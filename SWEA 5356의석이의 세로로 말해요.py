b77a3ef484685dae1082470238f5060bf82c37e2
chaselover <gkstmdwn234@gmail.com>
2021-08-15 18:58:27 +0900

    0815 daily algo

---
+++ SWEA 5356의석이의 세로로 말해요.py
@@ -0,0 +1,14 @@
+def read_vertical(s):
+    answer = ''
+    cnt = 0
+    while cnt <= 15:
+        for string in s:
+            if string:
+                answer += string.pop(0)
+        cnt+=1
+    return answer
+
+
+for test in range(1, int(input())+1):
+    strings = list(list(input()) for _ in range(5))
+    print(f'#{test} {read_vertical(strings)}')
