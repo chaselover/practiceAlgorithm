750cac8fc60a13f2bb5ab14a528c5b91e5be5a17
chaselover <gkstmdwn234@gmail.com>
2021-08-25 15:12:56 +0900

    0825 daily

--- 2422한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
+++ 2422한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
@@ -30,3 +30,25 @@ for _ in range(M):
 ans = 0
 dfs(0,0)
 print(ans)
+
+
+
+# 민구님 풀이 3개뽑는 총 조합 - 안되는 조합의 수
+# import sys
+# input = sys.stdin.readline
+
+# N, M = map(int, input().split())
+# worsts = set()
+# for _ in range(M):
+#     a, b = map(int, input().split())
+#     worst = 1<<(a-1)
+#     worst |= 1<<(b-1)
+#     for i in range(N):
+#         worsts.add(bin(worst|(1<<i)))
+
+# cnt = 0        
+# for worst in worsts:
+#     if worst.count('1') == 3:
+#         cnt = cnt + 1
+
+# print(N*(N-1)*(N-2)//6 - cnt)
