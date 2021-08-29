1119fca86c1014413173be0e91ddc5cc5d149bdc
chaselover <gkstmdwn234@gmail.com>
2021-08-29 13:52:41 +0900

    0829 daily algo

---
+++ 14621 나만 안되는 연애.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+def find(x):
+    if parent[x] == x:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a,b):
+    a = find(a)
+    b = find(b)
+
+    if level[a] >= level[b]:
+        parent[b] = a
+        if level[a]==level[b]:
+            level[a] +=1 
+    else:
+        parent[a] = b
+
+N, M = map(int, input().split())
+sex = [0] + list(input().split())
+arr = []
+parent = {i:i for i in range(1,N+1)}
+level = {i:0 for i in range(1,N+1)}
+for _ in range(M):
+    u, v, d = map(int, input().split())
+    arr.append([d,u,v])
+arr.sort()
+answer = 0
+for i in range(M):
+    dist,start,end = arr[i]
+    if find(start) != find(end) and sex[start] != sex[end]:
+        union(start,end)
+        answer += dist
+print(answer)
