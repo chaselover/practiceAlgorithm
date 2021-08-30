c033c6291256e3f36cc12a91168e9ebc8e03ccfa
chaselover <gkstmdwn234@gmail.com>
2021-08-31 01:56:31 +0900

    0831 daily algo

--- 카카오기출2018뉴스클러스터링.py
+++ 카카오기출2018뉴스클러스터링.py
@@ -1,33 +1,33 @@
 from collections import Counter
 
 def solution(str1, str2):
-    str1_low = str1.lower()
-    str2_low = str2.lower()
+    str1 = str1.lower()
+    str2 = str2.lower()
     
-    str1_lst = []
-    str2_lst = []
+    str1_elements_set = []
+    str2_elements_set = []
     # 두글자씩 끊어 원소집합으로 만듦(둘다 알파뱃일 경우만.)
-    for i in range(len(str1_low)-1):
-        if str1_low[i].isalpha() and str1_low[i+1].isalpha():
-            str1_lst.append(str1_low[i] + str1_low[i+1])
-    for j in range(len(str2_low)-1):
-        if str2_low[j].isalpha() and str2_low[j+1].isalpha():
-            str2_lst.append(str2_low[j] + str2_low[j+1])
+    for i in range(len(str1)-1):
+        if str1[i].isalpha() and str1[i+1].isalpha():
+            str1_elements_set.append(str1[i] + str1[i+1])
+    for j in range(len(str2)-1):
+        if str2[j].isalpha() and str2[j+1].isalpha():
+            str2_elements_set.append(str2[j] + str2[j+1])
     # 각 원소 집합에 대해 카운터 객체 생성.
-    Counter1 = Counter(str1_lst)
-    Counter2 = Counter(str2_lst)
+    Counter1 = Counter(str1_elements_set)
+    Counter2 = Counter(str2_elements_set)
     # 교집합(양쪽 다 원소의 갯수가 같은만큼만 원소추출)
-    inter = list((Counter1 & Counter2).elements())
+    inter_strs = list((Counter1 & Counter2).elements())
     # 합집합(모든 원소)
-    union = list((Counter1 | Counter2).elements())
+    union_strs = list((Counter1 | Counter2).elements())
     # 유사도는 교집합 크기/합집합 크기 * 65536 의 소숫점 버림.
     # ex
     # Counter({'aa': 2}) Counter({'aa': 3})
     # ['aa', 'aa'] ['aa', 'aa', 'aa']
     # 43690
-    if len(union) == 0 and len(inter) == 0:
+    if len(union_strs) == 0 and len(inter_strs) == 0:
         return 65536
     else:
-        return int(len(inter) / len(union) * 65536)
+        return int(len(inter_strs) / len(union_strs) * 65536)
 
 print(solution('aa1+aa2','AAAA12'))
---
+++ 카카오기출2018추석트래픽.py
@@ -0,0 +1,51 @@
+
+
+def get_times(log):
+    temp = log.split(" ")
+    end_time = temp[1].split(':')
+    end_time = int(end_time[0]) * 3600000 + int(end_time[1]) * 60000 + int(end_time[2].replace('.', ''))
+    start_time = end_time - int(float(temp[2].replace('s', ''))*1000) + 1
+    return (start_time, end_time) if start_time > 0 else (0, end_time)
+
+
+def solution(lines):
+    answer = 0
+    total_times = []
+    for i in range(len(lines)):
+        start_time, end_time = get_times(lines[i])
+        total_times.append((start_time, end_time, i))
+
+    for i in range(len(total_times)):
+        count = 1
+        end_time = total_times[i][1]
+        for j in range(len(total_times)):
+            if i == j:
+                continue
+            t_start_time = total_times[j][0]
+            t_end_time = total_times[j][1]
+            if t_start_time >= end_time and t_start_time < end_time + 1000:
+                count += 1
+            elif t_end_time >= end_time and t_end_time < end_time + 1000:
+                count += 1
+            elif end_time >= t_start_time and end_time + 1000 <= t_end_time:
+                count += 1
+        if count > answer:
+            answer = count
+
+    return answer
+
+
+
+
+print(solution([
+"2016-09-15 20:59:57.421 0.351s",
+"2016-09-15 20:59:58.233 1.181s",
+"2016-09-15 20:59:58.299 0.8s",
+"2016-09-15 20:59:58.688 1.041s",
+"2016-09-15 20:59:59.591 1.412s",
+"2016-09-15 21:00:00.464 1.466s",
+"2016-09-15 21:00:00.741 1.581s",
+"2016-09-15 21:00:00.748 2.31s",
+"2016-09-15 21:00:00.966 0.381s",
+"2016-09-15 21:00:02.066 2.62s"
+]))
