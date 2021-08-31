9caba86e329903a926de38e9e73a0d6c6057bfea
chaselover <gkstmdwn234@gmail.com>
2021-08-31 15:57:25 +0900

    0831 daily algo

---
+++ 1389케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+graph = [[float('inf') for _ in range(N+1)] for _ in range(N+1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+for k in range(1,N+1):
+    graph[k][k] = 0
+    for i in range(1,N+1):
+        for j in range(1,N+1):
+            if graph[i][j] > graph[i][k] + graph[k][j]:
+                graph[i][j] = graph[i][k] + graph[k][j]
+
+min_sum_user = float('inf')
+for i in range(1,N+1):
+    sum_friends = 0
+    for j in range(N+1):
+        if graph[i][j] != float('inf'):
+            sum_friends += graph[i][j]
+    if min_sum_user > sum_friends:
+        min_sum_user = sum_friends
+        answer = i
+print(answer)
