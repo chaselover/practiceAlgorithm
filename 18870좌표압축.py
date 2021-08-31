348529d4a871674a48ff4a887c6fcd23568f426b
chaselover <gkstmdwn234@gmail.com>
2021-08-31 15:29:14 +0900

    0831 daily algo

---
+++ 18870좌표압축.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+from bisect import bisect_left
+
+N = int(input())
+arr = list(map(int, input().split()))
+arr2  = sorted(list(set(arr)))
+for num in arr:
+    print(bisect_left(arr2,num),end=' ')
+
