b8a1a957658c15392045ac03f66de9537e67a850
chaselover <gkstmdwn234@gmail.com>
2021-09-03 23:40:24 +0900

    0903daily algo

--- 1068 트리.py
+++ 1068 트리.py
@@ -5,15 +5,17 @@ N = int(input())
 nodes = set([i for i in range(N)])
 childs = {i: [] for i in range(-1,N)}
 parents = list(map(int, input().split()))
+parent_node = set()
 for i in range(N):
     childs[parents[i]].append(i)
-parent_node = set(parents)
+    parent_node.add(parents[i])
 leafs = nodes - parent_node
-q = [int(input())]
+del_node = int(input())
+q = [del_node]
 while q:
     x = q.pop()
-    if not childs[x]:
-        leafs -= {x}
+    if x in leafs:
+        leafs.discard(x)
     for child in childs[x]:
         q.append(child)
-print(len(leafs))
+print(len(leafs)+1 if parents[del_node] != -1 and len(childs[parents[del_node]])==1 else len(leafs))
