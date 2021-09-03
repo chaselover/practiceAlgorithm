82d3a6bacc472e47c3d469475ff0f5119be75e7d
chaselover <gkstmdwn234@gmail.com>
2021-09-03 22:50:10 +0900

    daily algo

---
+++ 21966(중략).py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+s = input().rstrip()
+if N <= 25:
+    print(s)
+    exit()
+if '.' not in s[11:-12]:
+    print(s[:11]+'...'+s[-11:])
+    exit()
+print(s[:9] + '......' + s[-10:])
