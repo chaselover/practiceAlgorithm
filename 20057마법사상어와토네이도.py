7946ca29eb0a9b9c154ae88c3d3252105012ed11
chaselover <gkstmdwn234@gmail.com>
2021-09-05 17:09:45 +0900

    daily algo

--- 20057마법사상어와토네이도.py
+++ 20057마법사상어와토네이도.py
@@ -1,44 +1,110 @@
-import sys
-input = sys.stdin.readline
+# 몫과 나머지
+# 모래 계산하는 함수
+def recount(s_x, s_y, direction):
+    global ans
 
-def spread_sand(x,y,d):
-    total_sand = A[x][y]
-    out_sand = 0
-    A[x][y] = 0
-    try:
-        A[x+d][y+d] += int(total_sand*0.55)
-    except:
-        out_sand += int(total_sand*0.55)
+    if s_y < 0:
+        return
+        
+    # 3. a, out_sand
+    total = 0  # a 구하기 위한 변수
+    for dx, dy, z in direction:
+        nx = s_x + dx
+        ny = s_y + dy
+        if z == 0:  # a(나머지)
+            new_sand = sand[s_x][s_y] - total
+        else:  # 비율
+            new_sand = int(sand[s_x][s_y] * z)
+            total += new_sand
 
-
-
-def move(x,y,d,cnt,flag,sand):
-    if not x and not y:
-        print(sand)
-        exit()
-    for _ in range(cnt):
-        x += d_tor[d][0]
-        y += d_tor[d][1]
-        out_sand = spread_sand(x,y,d)
-    if flag:
-        flag ^= 1
-        cnt += 1
-    else:
-        flag ^= 1
-    move(x,y,(d+1)%4,cnt,flag,sand+out_sand)
-    
+        if 0 <= nx < N and 0 <= ny < N:  # 인덱스 범위이면 값 갱신
+            sand[nx][ny] += new_sand
+        else:  # 범위 밖이면 ans 카운트
+            ans += new_sand
 
 
 N = int(input())
-A = [list(map(int, input().split())) for _ in range(N)]
-# 토네이도는 matrix가운데부터 이동.
-# 토네이도가 이동한 칸의 모든 모래가 비율대로 흩어진다.(소숫점 아래는 버린다) - 총량에서 남은 모든 양은 a로 55퍼.
-# 토네이도는 1,1까지 이동 후 소멸.(N은 홀수.) / 가운데 칸 모래는 0으로 시작.
-# 격자 밖으로 나간 모래의 양을 출력.
-start_x, start_y = N//2, N//2
-cnt = 1
-# (0,-1), (1,0) 각 cnt번 이동 후 cnt += 1
-# (0,1), (-1,0) 각 cnt번 이동 후 cnt += 1
-d_tor = [(0,-1),(1,0),(0,1),(-1,0)]
-sand_ratio = [0.55,0.05,0.1,0.1,0.07,0.07,0.02,0.02,0.01,0.01]
-move(start_x,start_y,0,1,0,0)
+sand = [list(map(int, input().split())) for _ in range(N)]
+
+# 2. 방향별 모래 비율 위치
+left = [(1, 1, 0.01), (-1, 1, 0.01), (1, 0, 0.07), (-1, 0, 0.07), (1, -1, 0.1),
+         (-1, -1, 0.1), (2, 0, 0.02), (-2, 0, 0.02), (0, -2, 0.05), (0, -1, 0)]
+right = [(x, -y, z) for x,y,z in left]
+down = [(-y, x, z) for x,y,z in left]
+up = [(y, x, z) for x,y,z in left]
+
+s_x, s_y = N//2, N//2
+ans = 0
+dx = [0, 1, 0, -1]
+dy = [-1, 0, 1, 0]
+
+# 1.토네이도 회전 방향(y위치)
+dict = {0: left, 1: down, 2: right, 3: up}
+time = 0
+for i in range(2*N-1):
+    # 몫: i//4(타임+1), 나머지:i%4(방향)
+    d = i % 4
+    if d == 0 or d == 2:  # 다음 회차(d==0) 이거나 right(d==2) 이면 한번 더
+        time += 1
+    for _ in range(time):
+        n_x = s_x + dx[d]
+        n_y = s_y + dy[d]
+        recount(n_x, n_y, dict[d])  # y좌표, 방향
+        s_x, s_y = n_x, n_y
+
+print(ans)
+
+
+
+# 두 방향씩 묶어서.
+# 모래 계산하는 함수
+def recount(time, dx, dy, direction):
+    global ans, s_x, s_y
+
+    # y좌표 계산 & x좌표 갱신
+    for _ in range(time):
+        s_x += dx
+        s_y += dy
+        if s_y < 0:  # 범위 밖이면 stop
+            break
+
+        # 3. a, out_sand
+        total = 0  # a 구하기 위한 변수
+        for dx, dy, z in direction:
+            nx = s_x + dx
+            ny = s_y + dy
+            if z == 0:  # a(나머지)
+                new_sand = sand[s_x][s_y] - total
+            else:  # 비율
+                new_sand = int(sand[s_x][s_y] * z)
+                total += new_sand
+
+            if 0 <= nx < N and 0 <= ny < N:   # 인덱스 범위이면 값 갱신
+                sand[nx][ny] += new_sand
+            else:  # 범위 밖이면 ans 카운트
+                ans += new_sand
+
+
+N = int(input())
+sand = [list(map(int, input().split())) for _ in range(N)]
+
+# 2. 방향별 모래 비율 위치
+left = [(1, 1, 0.01), (-1, 1, 0.01), (1, 0, 0.07), (-1, 0, 0.07), (1, -1, 0.1),
+         (-1, -1, 0.1), (2, 0, 0.02), (-2, 0, 0.02), (0, -2, 0.05), (0, -1, 0)]
+right = [(x, -y, z) for x, y, z in left]
+down = [(-y, x, z) for x, y, z in left]
+up = [(y, x, z) for x, y, z in left]
+
+s_x, s_y = N//2, N//2  # 시작좌표(x좌표)
+ans = 0  # out_sand
+
+# 1.토네이도 회전 방향(y위치)
+for i in range(1, N + 1):
+    if i % 2:
+        recount(i, 0, -1, left)
+        recount(i, 1, 0, down)
+    else:
+        recount(i, 0, 1, right)
+        recount(i, -1, 0, up)
+
+print(ans)
