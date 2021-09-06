48b95050a758070278f76267269f894e8714b8e3
chaselover <gkstmdwn234@gmail.com>
2021-09-07 03:49:49 +0900

    daily algo

---
+++ 9375패션왕 신해빈.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+from collections import defaultdict
+
+for _ in range(int(input())):
+    n = int(input())
+    clothes = defaultdict(int)
+    ans = 1
+    for _ in range(n):
+        name, type_c = input().split()
+        clothes[type_c] += 1
+    for cloth in clothes:
+        ans *= clothes[cloth]+1
+    print(ans-1)
