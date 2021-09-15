0e2d2698279a7e3b05101d7af14c4c59e8c9f938
chaselover <gkstmdwn234@gmail.com>
2021-09-16 08:49:46 +0900

    dailya lgo

---
+++ 16208 귀찮음.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+total = sum(arr)
+answer  = 0
+for num in arr:
+    answer += num * (total - num)
+    total -= num
+print(answer)
