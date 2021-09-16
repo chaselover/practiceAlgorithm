7a84432654191cab6f38a0fa884b59d1de4db4ac
chaselover <gkstmdwn234@gmail.com>
2021-09-16 19:25:13 +0900

    daily algo

--- 1943 동전 분배.py
+++ 1943 동전 분배.py
@@ -1,16 +1,23 @@
 import sys
 input = sys.stdin.readline
-from collections import defaultdict
 
 for _ in range(3):
     N = int(input())
-    coins = defaultdict(int)
+    coins = {}
     target = 0
     for _ in range(N):
         a, b = map(int, input().split())
-        coins[a] += b
+        coins[a] = b
         target += a*b
     if target&1:
         print(0)
         continue
-    target //= 2 
+    target //= 2
+    dp = [1] + [0] * (target)
+    for coin in coins:
+        for money in range(target,coin-1,-1):
+            if dp[money-coin]:
+                for j in range(coins[coin]):
+                    if money + coin*j <= target:
+                        dp[money + coin*j] = 1
+    print(dp[target])
