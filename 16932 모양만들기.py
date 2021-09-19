4443076dabc85a1b8017eeea15b9a46ad53436ca
chaselover <gkstmdwn234@gmail.com>
2021-09-19 19:15:21 +0900

    study: Algo-itzy 15683감시

--- 16932 모양만들기.py
+++ 16932 모양만들기.py
@@ -5,20 +5,27 @@ from collections import deque
 def bfs(x,y):
     q = deque()
     visited[x][y] = True
-    q.append((x,y))
+    q.append((x,y,1))
     cnt = 1
     while q:
-        x, y = q.popleft()
+        x, y, key = q.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and matrix[nx][ny]:
-                visited[nx][ny] = True
-                q.append((nx,ny))
-                cnt += 1
-    return cnt
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
+                if matrix[nx][ny]:
+                    visited[nx][ny] = True
+                    q.append((nx,ny,key))
+                    cnt += 1
+                else:
+                    if key:
+                        visited[nx][ny] = True
+                        q.append((nx,ny,0))
+            
+    return cnt+1
 
 N, M = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(N)]
+visited = [[False]*M for _ in range(N)]
 
 dx = [0,0,-1,1]
 dy = [1,-1,0,0]
@@ -26,9 +33,6 @@ dy = [1,-1,0,0]
 max_ans = 0
 for i in range(N):
     for j in range(M):
-        if not matrix[i][j]:
-            visited = [[False]*M for _ in range(N)]
-            matrix[i][j] = 1
+        if matrix[i][j] and not visited[i][j]:
             max_ans = max(max_ans,bfs(i,j))
-            matrix[i][j] = 0
 print(max_ans)
