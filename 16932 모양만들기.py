a5baf5d9688493b087f4b72bd5ec4f59fc0b77af
chaselover <gkstmdwn234@gmail.com>
2021-09-20 12:54:33 +0900

    study: 0920) daily algorithm

--- 16932 모양만들기.py
+++ 16932 모양만들기.py
@@ -1,41 +1,69 @@
-import sys
-input = sys.stdin.readline
+from sys import stdin
 from collections import deque
 
-def bfs(x,y):
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+n,m = map(int, stdin.readline().strip().split())
+
+board = [list(map(int, stdin.readline().strip().split())) for _ in range(n)]
+candidate = []
+area_size = [0,0]
+
+def solve():
+    num = 2
+    for x in range(n):
+        for y in range(m):
+            if board[x][y] == 0:
+                candidate.append((x,y))
+            elif board[x][y] == 1:
+                insert_num(x,y,num)
+                num+=1
+
+    ans = -1
+    for x,y in candidate:
+        visited = set()
+        cnt = 1
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+
+            if not point_validator(nx, ny) or board[nx][ny] == 0:
+                continue
+            num = board[nx][ny]
+
+            if num in visited:
+                continue
+            visited.add(num)
+            cnt += area_size[num]
+        ans = max(ans, cnt)
+    print(ans)
+def insert_num(sx,sy,num):
+    global board, area_size
     q = deque()
-    visited[x][y] = True
-    q.append((x,y,1))
-    cnt = 1
+
+    q.appendleft((sx,sy))
+    board[sx][sy] = num
+    size = 1
+
     while q:
-        x, y, key = q.popleft()
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
-                if matrix[nx][ny]:
-                    visited[nx][ny] = True
-                    q.append((nx,ny,key))
-                    cnt += 1
-                else:
-                    if key:
-                        visited[nx][ny] = True
-                        q.append((nx,ny,0))
-            
-    return cnt+1
+        x,y = q.pop()
 
-N, M = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(N)]
-visited = [[False]*M for _ in range(N)]
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
 
-dx = [0,0,-1,1]
-dy = [1,-1,0,0]
+            if not point_validator(nx,ny) or board[nx][ny] != 1:
+                continue
 
-max_ans = 0
-for i in range(N):
-    for j in range(M):
-        if matrix[i][j] and not visited[i][j]:
-            max_ans = max(max_ans,bfs(i,j))
-print(max_ans)
+            board[nx][ny] = num
+            size += 1
+            q.appendleft((nx,ny))
 
+    area_size.append(size)
+def point_validator(x,y):
+    if x < 0 or y < 0 or x >= n or y >= m:
+        return False
+    return True
 
-
+solve()
---
+++ 5347LCM.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+from math import gcd
+
+
+for _ in range(int(input())):
+    a, b = map(int,input().split())
+    print(a*b//gcd(a,b))
