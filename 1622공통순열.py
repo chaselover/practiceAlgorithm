8cbab489da89bfcf42c813376a3e700932fcb981
chaselover <gkstmdwn234@gmail.com>
2021-09-23 23:35:37 +0900

    daily algo

---
+++ 1622공통순열.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+count = {}
+while 1:
+    check = ''
+    count_cur = {}
+    try:
+        a = input().rstrip()
+        for char in a:
+            if char not in count:
+                count[char] = 1
+            else:
+                check += char
+    except:
+        break
+    if check:
+        print(check)
---
+++ 17484진우의 달 여행.PY
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(N)]
+dp = [[[0,0,0] for _ in range(M)]] + [[[float('inf'),float('inf'),float('inf')] for _ in range(M)] for _ in range(N)]
+
+for i in range(1,N+1):
+    for j in range(M):
+        if j < M-1:
+            dp[i][j][0] = min(dp[i-1][j+1][1],dp[i-1][j+1][2]) + matrix[i-1][j]
+        if 0 < j:
+            dp[i][j][2] = min(dp[i-1][j-1][1],dp[i-1][j-1][0]) + matrix[i-1][j]
+        dp[i][j][1] = min(dp[i-1][j][0],dp[i-1][j][2]) + matrix[i-1][j]
+
+min_value = float('inf')
+for row in dp[i]:
+    for each in row:
+        min_value = min(min_value,each)
+print(min_value)
