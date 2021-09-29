d2ebd229137d851fd7c3acef5e373d6028ebae21
chaselover <gkstmdwn234@gmail.com>
2021-09-30 03:27:13 +0900

    daily algo

--- 17142 연구소3.py
+++ 17142 연구소3.py
@@ -5,10 +5,9 @@ from itertools import combinations
 
 
 def bfs(q):
-    visited = [[False] * N for _ in range(N)]
     for x, y in q:
         visited[x][y] = True
-    time = -1
+        times[x][y] = 0
     cnt = len(q)
     while q:
         for _ in range(len(q)):
@@ -18,9 +17,9 @@ def bfs(q):
                 if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and not matrix[nx][ny] == 1:
                     visited[nx][ny] = True
                     q.append((nx, ny))
+                    times[nx][ny] = times[x][y] + 1
                     cnt += 1
-        time += 1
-    return cnt, time
+    return cnt
 
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
@@ -42,11 +41,18 @@ for i in range(N):
 min_count = float('inf')
 for virus_set in combinations(initial_points, M):
     q = deque()
+    visited = [[False] * N for _ in range(N)]
+    times = [[-1] * N for _ in range(N)]
     for virus in virus_set:
         q.append(virus)
-    cnt, time = bfs(q)
+    cnt = bfs(q)
+    max_time = 0
+    for i in range(N):
+        for j in range(N):
+            if not matrix[i][j]:
+                max_time = max(max_time, times[i][j])
     if cnt + wall == N**2:
-        min_count = min(min_count, time)
+        min_count = min(min_count, max_time)
 if min_count == float('inf'):
     min_count = -1
 print(min_count)
