d0e761d4252568085443eeb982da1e994544aa63
chaselover <gkstmdwn234@gmail.com>
2021-09-29 23:50:51 +0900

    daily algo

---
+++ 17352 여러분의 다리가 되어 드리겠습니다.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+
+def find(x):
+    if parent[x] == x:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+
+    if a < b:
+        a, b = b, a
+    parent[b] = a
+
+
+N = int(input())
+parent = {i: i for i in range(1, N+1)}
+for _ in range(N-2):
+    a, b = map(int, input().split())
+    union(a, b)
+
+pivot = find(1)
+for i in range(2, N+1):
+    if pivot != find(i):
+        print(pivot, i)
+        exit()
