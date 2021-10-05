626ca90c851c4f5f2731a50ef531cb60066bbde5
chaselover <gkstmdwn234@gmail.com>
2021-10-06 06:13:15 +0900

    code: Solve swea L5208 전기버스2 (seungjoo)

---
+++ SWEA 5208 전기버스2.py
@@ -0,0 +1,11 @@
+
+
+for test in range(1, int(input()) + 1):
+    N, *battery = map(int, input().split())
+    dp = [float('inf')] * N
+    dp[N - 1] = 0
+    for i in range(N - 1, -1, -1):
+        for j in range(i - 1, -1, -1):
+            if j + battery[j] >= i:
+                dp[j] = min(dp[j], dp[i] + 1)
+    print(f'#{test} {dp[0] - 1}')
