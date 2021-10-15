a483973baaf8e94c2f7acc51470536feec649908
chaselover <gkstmdwn234@gmail.com>
2021-10-15 19:54:59 +0900

    code: boj 1920 수찾기 211015

--- 1920 수 찾기.py
+++ 1920 수 찾기.py
@@ -1,7 +1,15 @@
 import sys
 input = sys.stdin.readline
 
+num_set = {}
 N = int(input())
 A = list(map(int, input().split()))
+for num in A:
+    num_set[num] = True
 M = int(input())
 Q = list(map(int, input().split()))
+for num in Q:
+    if num in num_set:
+        print(1)
+    else:
+        print(0)
