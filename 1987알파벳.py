ddbe66ff7fdc5f2e6e8bdb6ad289e69be9252fd0
chaselover <gkstmdwn234@gmail.com>
2021-10-19 04:13:23 +0900

    solve: boj 1987 알파벳

--- 1987알파벳.py
+++ 1987알파벳.py
@@ -1,31 +1,27 @@
 import sys
 input = sys.stdin.readline
-# 좌, 하, 우, 상
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
 
 
-def BFS(x, y):
-    global answer
+def bfs(x, y):
     q = set([(x, y, board[x][y])])
-
+    check[x][y] = board[x][y]
+    answer = 1
     while q:
-        x, y, ans = q.pop()
-
-        # 좌우상하 갈 수 있는지 살펴본다
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            # index 벗어나지 않는지 체크하고, 새로운 칸이 중복되는 알파벳인지 체크한다
-            if ((0 <= nx < R) and (0 <= ny < C)) and (board[nx][ny] not in ans):
-                q.add((nx,ny,ans + board[nx][ny]))
-                answer = max(answer, len(ans)+1)
-
+        x, y, string = q.pop()
+        for dx, dy in delta:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < R and 0 <= ny < C and board[nx][ny] not in string:
+                new_string = string + board[nx][ny]
+                if check[nx][ny] != new_string:
+                    check[nx][ny] = new_string
+                    q.add((nx, ny, new_string))
+                    answer = max(answer, len(string) + 1)
+                    if answer == 26:
+                        return 26
+    return answer
 
 R, C = map(int, input().split())
-board = [list(input().strip()) for _ in range(R)]
-
-answer = 1
-BFS(0, 0)
-print(answer)
+board = [list(input().rstrip()) for _ in range(R)]
+delta = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+check = [[''] * C for _ in range(R)]
+print(bfs(0, 0))
