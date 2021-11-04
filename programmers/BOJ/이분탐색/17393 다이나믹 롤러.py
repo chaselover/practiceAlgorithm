8e04c6ae331e159e3b64f324b8795f5a5a7e15b3
chaselover <gkstmdwn234@gmail.com>
2021-11-04 23:00:11 +0900

    solve: BOJ 17393 다이나믹 룰러

---
+++ programmers/BOJ/이분탐색/17393 다이나믹 롤러.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+from bisect import bisect_right
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+answer = []
+for idx, a in enumerate(A):
+    answer.append(bisect_right(B, a) - idx - 1)
+print(*answer)
